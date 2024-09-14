4dc22571663027bef6fea56ad17f4bf2b1f5ec9f
alya852 <pstachiosh565@gmail.com>
2024-09-14 10:03:30 +0700

    Add files via upload

---
+++ Program menghitung luas persegi panjang.cpp
@@ -0,0 +1,15 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    int panjang, lebar, luas;// Write C++ code here
+    cout << "Masukkan panjang = ";
+    cin >> panjang;
+    cout << "Masukkan lebar = ";
+    cin >> lebar;
+    luas = panjang*lebar;
+    cout << "Luasnya adalah = " << luas << endl;
+
+    return 0;
+}
